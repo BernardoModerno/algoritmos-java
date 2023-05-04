cd329c09ea4d720e275906e138dee1a9cf933156
BernardoModerno <bernardo.moderno@gmail.com>
2023-05-03 21:12:37 -0300

    Montando primeira fila

--- queue/src/Main.java
+++ queue/src/Main.java
@@ -1,9 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        
+
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Harold");
+
+        System.out.println(queue);
     }
 }
